ac2e5acf643573567bf77e05654d48c15648c54e
Naresh Bhatia <me@nareshbhatia.dev>
2024-07-16 00:19:32 -0400

    feat: add dummy movie table

--- apps/movie-magic-ultimate/src/app/movies/page.tsx
+++ apps/movie-magic-ultimate/src/app/movies/page.tsx
@@ -1,7 +1,32 @@
+import { movies } from './movies';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
 export default function MoviesPage() {
   return (
     <div className="container relative mx-auto max-w-screen-xl px-8 py-4">
-      Movies
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead className="w-[80px]">Cert</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {movies.map((movie) => (
+            <TableRow key={movie.id}>
+              <TableCell className="font-medium">{movie.name}</TableCell>
+              <TableCell>{movie.certificate}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
