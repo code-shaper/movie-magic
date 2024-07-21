2bcdcc206744f72571a5fc5eaeb383fdefb3a6a8
Naresh Bhatia <me@nareshbhatia.dev>
2024-07-21 15:32:44 -0400

    chore: remove <head /> element from layout.tsx (not needed in app router)

--- apps/movie-magic-ultimate/src/app/layout.tsx
+++ apps/movie-magic-ultimate/src/app/layout.tsx
@@ -82,7 +82,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
       lang="en"
       suppressHydrationWarning
     >
-      <head />
       <body>
         <AppProvider baseApiUrl={baseApiUrl} useMockData={useMockData}>
           <div className="relative flex min-h-screen flex-col">
