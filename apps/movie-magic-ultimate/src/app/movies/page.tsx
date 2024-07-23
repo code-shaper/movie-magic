7ec7558d13932a82eeb77d96a2a43e90ed33e618
Naresh Bhatia <me@nareshbhatia.dev>
2024-07-23 13:38:49 -0400

    fix: fix table styling

--- apps/movie-magic-ultimate/src/app/movies/page.tsx
+++ apps/movie-magic-ultimate/src/app/movies/page.tsx
@@ -44,20 +44,34 @@ export default async function MoviesPage() {
   });
   const { movies: moviesResponse } = data;
 
+  /*
+   * Table styling
+   * -------------
+   * <Table>
+   *   Set to `table-fixed` to prevent expanding beyond the container width
+   *   to fit content
+   * <TableHead> for Cert column
+   *   Set to `w-[80px]` to fix its width to 80px
+   * <TableCell> for Name column
+   *   Set to `truncated` to truncate long text while allowing it to expand
+   *   within the fixed-width table
+   */
   return (
     <div className="container relative mx-auto max-w-screen-xl px-8 py-4">
-      <Table>
+      <Table className="table-fixed">
         <TableHeader>
           <TableRow>
             <TableHead>Name</TableHead>
-            <TableHead className="w-[80px]">Cert</TableHead>
+            <TableHead className="w-[80px] text-center">Cert</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {moviesResponse.movies.map((movie) => (
             <TableRow key={movie.id}>
-              <TableCell className="font-medium">{movie.name}</TableCell>
-              <TableCell>
+              <TableCell className="truncate font-medium">
+                {movie.name}
+              </TableCell>
+              <TableCell className="text-center">
                 {convertCertificateRating(movie.certificate.rating)}
               </TableCell>
             </TableRow>
