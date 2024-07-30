a3f515eb5c637060ffa51b793a1cf22b14783723
Naresh Bhatia <me@nareshbhatia.dev>
2024-07-30 03:13:19 -0400

    fix: further reduce filter panel scroll area

--- apps/movie-magic-ultimate/src/app/(members)/movies/_components/FilterPanel.tsx
+++ apps/movie-magic-ultimate/src/app/(members)/movies/_components/FilterPanel.tsx
@@ -176,7 +176,7 @@ export function FilterPanel({
   return (
     <MoviesRequestContextProvider initialState={moviesRequest}>
       <Header onApplyFilters={onApplyFilters} onClearFilters={onClearFilters} />
-      <ScrollArea className="h-[calc(100vh-14rem)]">
+      <ScrollArea className="h-[calc(100vh-15rem)]">
         <div className="space-y-8">
           <Search />
           <GenreFilter />
