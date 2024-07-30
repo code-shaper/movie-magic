607d90286a8528fc6a11cc11a21946efe72b4b0a
Naresh Bhatia <me@nareshbhatia.dev>
2024-07-30 15:48:22 -0400

    feat: add animation to home page

--- apps/movie-magic-ultimate/tailwind.config.ts
+++ apps/movie-magic-ultimate/tailwind.config.ts
@@ -76,10 +76,20 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'slide-right': {
+          from: { transform: 'translateX(-20px)', opacity: '0' },
+          to: { transform: 'translateX(0px)', opacity: '1' },
+        },
+        'slide-top': {
+          from: { transform: 'translateY(20px)', opacity: '0' },
+          to: { transform: 'translateY(0px)', opacity: '1' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'slide-right': 'slide-right 0.3s ease-out',
+        'slide-top': 'slide-top 0.3s ease-out',
       },
     },
   },
