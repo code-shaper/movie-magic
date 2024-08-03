a714f26e4a977e41cb8e0607e9e78e1f0e39e112
Naresh Bhatia <me@nareshbhatia.dev>
2024-08-03 17:06:03 -0400

    fix: fix alignment issue with user menu + add username & email

--- apps/movie-magic-ultimate/src/components/AppHeader/UserMenu.tsx
+++ apps/movie-magic-ultimate/src/components/AppHeader/UserMenu.tsx
@@ -1,11 +1,14 @@
 import { SignInLink } from './SignInLink';
 import { SignOutButton } from '@/components/Auth';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
+  DropdownMenuLabel,
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
 } from '@/components/ui/dropdown-menu';
 import { auth } from '@/lib/auth';
 
@@ -13,6 +16,7 @@ export async function UserMenu() {
   const session = await auth();
 
   const username = session?.user?.name ?? '?';
+  const userEmail = session?.user?.email ?? undefined;
   const userImage = session?.user?.image ?? undefined;
 
   // Extract user initials
@@ -28,12 +32,25 @@ export async function UserMenu() {
       {session ? (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Avatar className="size-8">
-              <AvatarImage src={userImage} />
-              <AvatarFallback>{userInitials}</AvatarFallback>
-            </Avatar>
+            <Button className="relative size-8 rounded-full" variant="ghost">
+              <Avatar className="size-8">
+                <AvatarImage alt={username} src={userImage} />
+                <AvatarFallback>{userInitials}</AvatarFallback>
+              </Avatar>
+            </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent>
+          <DropdownMenuContent align="end" className="w-56" forceMount>
+            <DropdownMenuLabel className="font-normal">
+              <div className="flex flex-col space-y-1">
+                <p className="text-sm font-medium leading-none">{username}</p>
+                {userEmail !== undefined ? (
+                  <p className="text-xs leading-none text-muted-foreground">
+                    {userEmail}
+                  </p>
+                ) : undefined}
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
             <DropdownMenuItem>
               <SignOutButton redirectTo="/" />
             </DropdownMenuItem>
